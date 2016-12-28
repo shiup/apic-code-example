66dc9aac7730d64d735faabe626356820c6fe0b9
shiup <shiup@hotmail.com>
2016-12-28 14:38:40 -0500

    Update basic-auth.js

--- gatewayscript/basic-auth.js
+++ gatewayscript/basic-auth.js
@@ -1,4 +1,6 @@
 // author : @spoon or @shiup
+// for more information on authenticate-url
+//     http://www.ibm.com/support/knowledgecenter/SSFS6T/com.ibm.apic.toolkit.doc/con_auth_url.html#con_auth_url
 
 var apic = require('./apim.custom.js');
 apic.output('application/json');
@@ -12,7 +14,7 @@ apic.console.debug('user credential : [' + username + ':' + password + ']');
 
 if (username === apic.getvariable('request.parameters.username') && password === apic.getvariable('request.parameters.password')) {
     session.output.write({"authenticatedUser":username});
-    apic.setvariable('message.headers.apic-authenticated-credential', 'cn=' + username + ',email=' + username + '@poon.com,c=us');
+    apic.setvariable('message.headers.api-authenticated-credential', 'cn=' + username + ',email=' + username + '@poon.com,c=us');
     apic.setvariable('message.status.code', 200)
 }
 else {
